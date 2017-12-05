4c984353bb7cf24cc1d1ce08ffe607642b13a055
ShenNuo <33053488+shennuo@users.noreply.github.com>
2017-12-05 16:28:52 +0800

    Update TeseOne.java

--- TeseOne.java
+++ TeseOne.java
@@ -2,7 +2,6 @@ public class TestOne
   public static void main(String args[]){
      int a=3;
      int b=4;
-     System.out.println(a+b);
-     System.out.println(a-b);
+     System.out.println(a*b);
      }
 }
