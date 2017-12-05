7fa0e532b9da6b4ef344cd799eafd4a857f00766
Administrator <Administrator@D42>
2017-12-05 16:41:56 +0800

    This is seond

--- TeseTwo.java
+++ TeseTwo.java
@@ -2,6 +2,7 @@ public class TestOne
   public static void main(String args[]){
      int a=3;
      int b=4;
-     System.out.println(a*b);   
+     System.out.println(a*b); 
+     System.out.println(a/b);
      }
 }
